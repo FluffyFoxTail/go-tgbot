530c10647bcdba5d1ebe7d1bd7a9144e45f3c3fc
Denis <fluffyfoxtail988@gmail.com>
2022-10-09 14:00:18 -0400

    added help command to bot

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"tgbot/internal/config"
 	"tgbot/internal/logging"
 	"tgbot/pkg/tgbot"
 )
 
-// TODO implement bot Start/Stop and listen methonds
+// TODO REPLACE panic to beaty message
 func main() {
 	logger := logging.NewLogger()
 
@@ -19,7 +24,25 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	_ = bot
-	// TODO
+	logger.Info("Start up bot")
+	bot.Start(ctx)
+
+	logger.Info("Bot on listen updates")
+	defer bot.Stop()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	defer signal.Stop(interrupt)
+
+	select {
+	case signal := <-interrupt:
+		logger.Warn(fmt.Sprintf("Signal was caught, app is stop: %v", signal))
+		cancel()
+	case <-ctx.Done():
+		logger.Warn(fmt.Sprintf("Context was close, app is stop: %s", ctx.Err()))
+		return
+	}
 }
--- pkg/tgbot/bot.go
+++ pkg/tgbot/bot.go
@@ -1,6 +1,7 @@
 package tgbot
 
 import (
+	"context"
 	"fmt"
 	"tgbot/internal/config"
 	"tgbot/internal/logging"
@@ -32,4 +33,65 @@ func NewBot(config *config.Config, logger *logging.BotLogger) (*Bot, error) {
 
 	logger.Info(fmt.Sprintf("Log in on: %s", api.Self.UserName))
 	return &Bot{api: api, config: config, logger: logger}, nil
+}
+
+func (b *Bot) Start(ctx context.Context) {
+	update := tgbotapi.NewUpdate(0)
+	update.Timeout = b.config.Timeout
+	updates := b.api.GetUpdatesChan(update)
+	go b.listen(ctx, updates)
+}
+
+func (b *Bot) listen(ctx context.Context, updates tgbotapi.UpdatesChannel) {
+	for {
+		select {
+		case <-ctx.Done():
+			b.logger.Warn(fmt.Sprintf("Msg conn closed: %v", ctx.Err()))
+			return
+		case u := <-updates:
+			if u.Message == nil {
+				continue
+			}
+
+			if !u.Message.IsCommand() {
+				b.reply(u.Message.Chat.ID, u.Message.MessageID, "ыыыы: "+u.Message.Text)
+				continue
+			}
+
+			switch u.Message.Command() {
+			case "hello":
+				go b.hello(u.Message)
+			case "help":
+				go b.help(u.Message)
+			default:
+				go b.reply(u.Message.Chat.ID, 0, "I dont now this command")
+			}
+		}
+	}
+}
+
+func (b *Bot) Stop() {
+	b.api.StopReceivingUpdates()
+}
+
+// TODO implement in config method get for commands
+func (b *Bot) hello(msg *tgbotapi.Message) {
+	b.reply(msg.Chat.ID, msg.MessageID, "hello!!!!")
+}
+
+func (b *Bot) help(msg *tgbotapi.Message) {
+	b.reply(msg.Chat.ID, msg.MessageID, "i`m just learning now!")
+}
+
+func (b *Bot) reply(chatId int64, msgID int, text string) {
+	b.logger.Info(fmt.Sprintf("Sending reply for: %d", chatId))
+	msg := tgbotapi.NewMessage(chatId, fmt.Sprint(text))
+	if msgID != 0 {
+		msg.ReplyToMessageID = msgID
+	}
+
+	if _, err := b.api.Send(msg); err != nil {
+		b.logger.Error(fmt.Sprintf("Unable to send msg: %v", err))
+		return
+	}
 }
